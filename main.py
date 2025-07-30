17cb7b17f2db87d7dfc73523a97cd1f1bce3d3a2
richylv <drafther@gmail.com>
2025-07-29 20:36:55 -0600

    send email

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import cv2
 import time
+import emailing
+from emailing import send_emai
 
 video = cv2.VideoCapture(0)
 time.sleep(1)
 
 first_frame = None
+status_list = []
 
 while True:
+    status = 0
     check, frame = video.read()
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray_frame_gau = cv2.GaussianBlur(gray_frame,(21,21),0)
@@ -26,7 +30,16 @@ while True:
         if cv2.contourArea(contour) < 5000:
             continue
         x, y, w, h = cv2.boundingRect(contour)
-        cv2.rectangle(frame,(x, y), (x+w, y+h),(0, 255, 0), 3)
+        rectangle = cv2.rectangle(frame,(x, y), (x+w, y+h),(0, 255, 0), 3)
+        if rectangle.any():
+            status = 1
+
+
+    status_list.append(status)
+    status_list = status_list[-2:]
+
+    if status_list[0] == 1 and status_list[1] == 0:
+        send_emai()
 
     cv2.imshow("video",frame)
     key = cv2.waitKey(1)
